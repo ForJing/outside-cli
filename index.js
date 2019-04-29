178dd043d3e965e5eeeae0bea3f6bebd2656fb24
forjing <xiaodoubao1994@gmail.com>
2019-04-29 14:40:09 +0800

    add minimist

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const minimist = require("minimist");
+
 module.exports = () => {
-  console.log("welcome to the outside");
+  const args = minimist(process.argv.slice(2));
+  console.log(args);
 };
