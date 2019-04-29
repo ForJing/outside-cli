86afa10b38758f9df15163974badffbcbaaf8adf
forjing <xiaodoubao1994@gmail.com>
2019-04-29 14:47:54 +0800

    add cmds

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ const minimist = require("minimist");
 module.exports = () => {
   const args = minimist(process.argv.slice(2));
   console.log(args);
+
+  const cmd = args._[0];
+
+  switch (cmd) {
+    case "today":
+      require("./cmds/today.js")(args);
+      break;
+    default:
+      console.error(`"${cmd}" is not a valid command`);
+      break;
+  }
 };
