70d79fc472f3478bcfb2c9b315f2ca6ed61b6fbe
forjing <xiaodoubao1994@gmail.com>
2019-04-29 15:39:45 +0800

    add version help cmd

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const minimist = require("minimist");
+const chalk = require("chalk");
 
 module.exports = () => {
   const args = minimist(process.argv.slice(2));
-  console.log(args);
 
-  const cmd = args._[0];
+  let cmd = args._[0] || "help";
+
+  if (args.version || args.v) {
+    cmd = "version";
+  }
+
+  if (args.help || args.h) {
+  }
 
   switch (cmd) {
     case "today":
       require("./cmds/today.js")(args);
       break;
+    case "version":
+      require("./cmds/version.js")(args);
+      break;
+    case "help":
+      require("./cmds/help.js")(args);
+      break;
     default:
-      console.error(`"${cmd}" is not a valid command`);
+      console.error(`"${cmd}" is not a valid command!`);
       break;
   }
 };
